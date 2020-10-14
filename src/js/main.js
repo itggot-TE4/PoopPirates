9d9751f19f3a8c0b44f414539f309b9aeacf6063
Te4-Dennis-Christensen <dennis.christensen@elev.ga.ntig.se>
2020-10-14 14:14:44 +0200

    added repo card template and made a function for creating a card given the repo data

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,16 @@ function onLoad(){
     
 }
 
-onLoad()
+function repoCardCreate(data){
+    let card = document.querySelector('#templateRepo').content.cloneNode(true).querySelector('.repoCard')
+    card.querySelector('.repoName').appendChild(document.createTextNode(data.name))
+    card.querySelector('.forksLink').href = data.forks_url
+    card.querySelector('.gitLink').href = data.html_url
+    card.querySelector('.forksCounter').appendChild(document.createTextNode(data.forks))
+    return card
+}
 
+onLoad()
 function searchRequest() {
     const input = document.querySelector('#searchbar').value;
     const result = await fetch(`https://api.github.com/users/${input}/repo`);
